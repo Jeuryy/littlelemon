2a6f8f8a3f86f1ce9bffc6e42109ce5861136669
Jeuryy <elyruej.102004@gmail.com>
2024-12-18 22:15:40 -0400

    form basics

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -3,8 +3,14 @@ import './BookingForm.css'
 
 const BookingForm = () => {
 const [formData, setFormData] = useState({
-    name: ''
+    date: '',
+    time: '',
+    guests: 1,
+    occasion: '',
+    mobile: '',
+    email: ''
 });
+const [error, setError] = useState('')
 
 const handleChange = (e) => {
     const {name, value} = e.target;
@@ -12,27 +18,81 @@ const handleChange = (e) => {
         ...formData,
         [name]: value
     });
-    console.log(formData)
 };
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
+
+    if (!formData.date || !formData.time || !formData.guests
+        || !formData.occasion || !formData.mobile || !formData.email)
+    {
+        setError('All fields are required!')
+        return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(formData.email)) {
+        setError('Por favor ingrese un correo electrónico válido');
+        return;
+    }
+
+    setError('');
+    console.log('Form sent');
+    console.log(formData);
 }
 
     return (
-        <div>
-            <h2>Book a reservaton</h2>
+        <div className='booking-form-container'>
+            <h1>Book a reservation!</h1>
             <form>
-                <label htmlFor='name'>Name</label>
+                <label htmlFor='date'>Date</label>
+                <input
+                    type='date'
+                    name='date'
+                    id='date'
+                    value={formData.date}
+                    onChange={handleChange}/>
+                <label htmlFor='time'>Time</label>
+                <input
+                    type='time'
+                    name='time'
+                    id='time'
+                    value={formData.time}
+                    onChange={handleChange}/>
+                <label htmlFor='guests'>Guests</label>
+                <input
+                    type='range'
+                    min={1}
+                    max={10}
+                    name='guests'
+                    id='guests'
+                    value={formData.guests}
+                    onChange={handleChange}/>
+                <label htmlFor='occasion'>Select an occasion</label>
+                <select
+                    id='occasion'
+                    name='occasion'
+                    value={formData.occasion}
+                    onChange={handleChange}>
+                    <option value="Birthday">Birthday</option>
+                    <option value="Anniversary">Anniversary</option>
+                </select>
+                <label htmlFor='mobile'>Mobile</label>
                 <input
                     type='text'
-                    name='name'
-                    id='name'
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder='Name'/>
+                    name='mobile'
+                    id='mobile'
+                    value={formData.mobile}
+                    onChange={handleChange}/>
+                <label htmlFor='email'>Email</label>
+                <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    value={formData.email}
+                    onChange={handleChange}/>
                 <button type='submit' onClick={handleSubmit}>Book</button>
+                {error && <p>{error}</p>}
             </form>
         </div>
     )
